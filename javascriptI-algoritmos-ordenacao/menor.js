7b8e41c1aa906ba865a30777eeb41c3671448619
Elvis Lira <professorelvislira@gmail.com>
2022-05-12 21:44:14 -0300

    Javascript - Algoritmos de ordenação - lista de livros

--- javascriptI-algoritmos-ordenacao/menor.js
+++ javascriptI-algoritmos-ordenacao/menor.js
@@ -1,15 +1,17 @@
-const listaNumeros = [50, 15, 25, 35, 6, 45]
+import { tabelaLivros } from "./lista-livros.js"
+
+// const listaNumeros = [50, 15, 25, 35, 6, 45]
 
 let indiceMenor = 0
 let indiceAtual
 
-for (let indice = 0; indice < listaNumeros.length; indice++) {
+for (let indice = 0; indice < tabelaLivros.length; indice++) {
     indiceAtual = indice
 
-    if (listaNumeros[indiceAtual] < listaNumeros[indiceMenor]) {
+    if (tabelaLivros[indiceAtual].preco < tabelaLivros[indiceMenor].preco) {
         indiceMenor = indiceAtual
         indiceAtual = indice
     }
 }
 
-console.log(listaNumeros[indiceMenor])
+console.log(tabelaLivros[indiceMenor].preco)
