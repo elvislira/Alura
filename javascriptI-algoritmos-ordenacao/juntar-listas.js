4743bb99c80d2617d8a2e7ec55e9787fbb7d4ca1
Elvis Lira <professorelvislira@gmail.com>
2022-05-13 21:49:00 -0300

    Javascript - Algoritmos de ordenação - Juntar listas.

---
+++ javascriptI-algoritmos-ordenacao/juntar-listas.js
@@ -0,0 +1,39 @@
+import { edFolha, edGalho } from "./listas.js"
+
+export const juntarListas = (lista1, lista2) => {
+    let listaUnica = []
+    let posicaoAtualLista1 = 0
+    let posicaoAtualLista2 = 0
+    let posicaoAtualListaUnica = 0
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let itemAtualLista1 = lista1[posicaoAtualLista1]
+        let itemAtualLista2 = lista2[posicaoAtualLista2]
+
+        if (itemAtualLista1.preco < itemAtualLista2.preco) {
+            listaUnica.push(itemAtualLista1)
+            posicaoAtualLista1++
+        } else {
+            listaUnica.push(itemAtualLista2)
+            posicaoAtualLista2++
+        }
+
+        posicaoAtualListaUnica++
+    }
+
+    while (posicaoAtualLista1 < lista1.length) {
+        listaUnica.push(lista1[posicaoAtualLista1])
+        posicaoAtualLista1++
+        posicaoAtualListaUnica++
+    }
+
+    while (posicaoAtualLista2 < lista2.length) {
+        listaUnica.push(lista2[posicaoAtualLista2])
+        posicaoAtualLista2++
+        posicaoAtualListaUnica++
+    }
+
+    return listaUnica
+}
+
+console.log(juntarListas(edFolha, edGalho))
