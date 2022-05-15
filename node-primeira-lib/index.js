76ea0d67b6d51b00b0c2fdd67b4bd6d1543a9895
Elvis Lira <professorelvislira@gmail.com>
2022-05-15 12:29:12 -0300

    NodeJS - Primeira biblioteca - regex match

--- node-primeira-lib/index.js
+++ node-primeira-lib/index.js
@@ -1,6 +1,17 @@
 import chalk from "chalk"
 import { readFile, promises } from "fs"
 
+const texto = 'A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo. São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
+
+function extrairLinks(texto) {
+    const regex = /\[[^\]]*\]\((https?:\/\/[^$#\s]*)\)/gm
+    const linksExtraidos = texto.match(regex)
+
+    console.log(linksExtraidos)
+}
+
+extrairLinks(texto)
+
 function tratarErro(erro) {
     throw new Error(chalk.red(`${erro.code} Erro ao tentar ler o arquivo!`))
 }
@@ -36,4 +47,6 @@ function lerArquivoSemPromise(caminhoArquivo) {
     })
 }
 
-console.log(lerArquivoAsyncAwait('./arquivos/texto1.md'))
+// console.log(lerArquivoAsyncAwait('./arquivos/texto1.md'))
+
+//  \[[^\]]*\]\((https?:\/\/[^$#\s]*)\)
