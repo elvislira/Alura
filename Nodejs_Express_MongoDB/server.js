0edca04a132a2fb8637738618c7dbe4c1a483b2c
Elvis Lira <professorelvislira@gmail.com>
2022-05-28 19:25:50 -0300

    GET com express

--- Nodejs_Express_MongoDB/server.js
+++ Nodejs_Express_MongoDB/server.js
@@ -1,20 +1,8 @@
 import { createServer } from 'http';
+import app from './src/app.js';
 
-const porta = 3000;
+const porta = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso de NodeJS',
-    '/livros': 'Entrei na página de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Página de editora',
-    '/sobre': 'Info sobre o projeto'
-}
-
-const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(porta, () => {
+app.listen(porta, () => {
     console.log(`Servidor escutando em http://localhost:${porta}`)
 })
