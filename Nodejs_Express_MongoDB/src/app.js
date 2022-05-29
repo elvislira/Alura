2c5b82fe2a5588cfe2c5263d811c7a7f48b0390b
Elvis Lira <professorelvislira@gmail.com>
2022-05-29 12:07:19 -0300

    NodeJS - Método DELETE (excluindo livro)

--- Nodejs_Express_MongoDB/src/app.js
+++ Nodejs_Express_MongoDB/src/app.js
@@ -14,44 +14,7 @@ db.once(
 );
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-// const livros = [
-//     {id: 1, 'titulo': 'Senhor dos Aneis'},
-//     {id: 2, 'titulo': 'O Hobbit'}
-// ];
-
-
-app.get('/livros/:id', (req, res) => {
-    let indice = buscaLivro(req.params.id);
-
-    res.json(livros[indice]);
-});
-
-// Alteração
-app.put('/livros/:id', (req, res) => {
-    let indice = buscaLivro(req.params.id);
-
-    livros[indice].titulo = req.body.titulo;
-
-    res.json(livros);
-});
-
-// Exclusão
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let indice = buscaLivro(id);
-
-    livros.splice(indice, 1);
-
-    res.send(`Livro ${id} removido com sucesso.`);
-});
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id);
-}
 
 export default app;
-
